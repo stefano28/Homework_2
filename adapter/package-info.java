f4b02c6c6bc636b3dbb5734dd5298c5fe2784e14
stefano <stefano@DESKTOP-39ASO4H.station>
2020-06-06 15:43:50 +0200

    package-info

---
+++ adapter/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides the classes for adapting HMap, HSet, HList and HCollection. 
+ */
+package adapter;
---
+++ adapter/test/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides the test case classes for MapAdapter, SetAdapter, ListAdapter and CollectionAdapter. 
+ */
+package adapter.test;
